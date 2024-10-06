298aeb5dff1ac32aedf9b5d919cde71053146fc6
MattBailey <mattbaileywork@gmail.com>
2024-10-06 11:54:01 -0600

    finished unit 13 and 14

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD4DIP9lavhAPIwoy8yfEk31-eNHJqvgNs",
+        authDomain: "hr-timesheet-974ac.firebaseapp.com",
+        projectId: "hr-timesheet-974ac",
+        storageBucket: "hr-timesheet-974ac.appspot.com",
+        messagingSenderId: "496416404629",
+        appId: "1:496416404629:web:2e390ac419efc2dc85179c",
+        measurementId: '<measurementId goes here>'
+    },
+  };
